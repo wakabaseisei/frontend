c9fab637e8dcb0be6693862ddf63f37706a64524
wakabayashi <wakabaseisei@gmail.com>
2022-02-17 16:07:51 +0900

    GKEの動作確認用にリクエストを叩くURLを変更
    エフェメラルなIPなので、そのまま使えない

--- frontend/app/src/Config/const.ts
+++ frontend/app/src/Config/const.ts
@@ -1,4 +1,8 @@
-export const BASE_URI = "http://localhost:30090";
+// 本番環境用のURL
+// 動作確認のため、ハードコーディングしている。
+// TODO: 開発環境用と本番環境用で分ける必要がある
+// TODO: エフェメラルなIPなので、都度変える前提
+export const BASE_URI = "http://34.146.197.140:30090";
 
 export enum ResponseStatusCode {
   Success = 200,
